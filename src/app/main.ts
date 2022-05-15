d565a73492e1547867728161262b839e9d1bae90
Sh4rkD <20006132@es.uveg.edu.mx>
2022-05-15 11:49:07 -0600

    utilizando faker para rellenar datos y empezando crud

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,27 @@
-import { addProduct } from './products/product.service';
+import faker from "@faker-js/faker"
+import { addProduct,products } from './products/product.service';
 
-addProduct({
-    id: '1',
-    title: 'Product 1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    stock: 10,
-    size: 'M',
-    category: {
-        id: '1',
-        name: 'Category 1',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-});
+for (let i = 0; i < 50; i++) {
+    addProduct({
+        id: faker.datatype.uuid(),
+        description: faker.commerce.productDescription(),
+        image: faker.image.imageUrl(),
+        color: faker.commerce.color(),
+        size: faker.random.arrayElement(["S", "M", "L", "XL"]),
+        price: parseInt(faker.commerce.price()),
+        isNew: faker.datatype.boolean(),
+        tags: [faker.commerce.productAdjective(), faker.commerce.productMaterial()],
+        title: faker.commerce.productName(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+        stock: faker.datatype.number({ min: 10, max: 100 }),
+        category: {
+            id: faker.datatype.uuid(),
+            name: faker.commerce.department(),
+            createdAt: faker.date.recent(),
+            updatedAt: faker.date.recent()
+        }
+    });
+}
+
+console.log(products);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,8 @@ export const products: Product[] = [];
 export const addProduct = (product: Product): void => {
     products.push(product);
 };
+
+export const updateProduct = (id: string | number, product: Product): void => {
+    const index = products.findIndex(p => p.id === id);
+    products[index] = product;
+}
